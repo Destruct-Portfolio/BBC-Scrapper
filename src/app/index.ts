55b6cbb73e75c4754ef263870190ceb0e5d9cd9e
Ben Hichem <77390862+benhichem@users.noreply.github.com>
2022-12-03 08:49:27 -0500

    Update index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -20,7 +20,7 @@ TimeConfig.hour = [0, new schedule.Range(0, 23)];
 
 TimeConfig.minute = 0;
 
-await Index.start();
+//await Index.start();
 
 //lights
 var job = schedule.scheduleJob(TimeConfig, async () => {
